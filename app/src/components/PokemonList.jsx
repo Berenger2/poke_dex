d01ca4a93e85895fe6839237e420a85c19814ff5
AKO_Berenger <vlunaski@gmail.com>
2024-05-13 23:23:37 +0200

    ✨(app) buid scroll infini

--- app/src/components/PokemonList.jsx
+++ app/src/components/PokemonList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import PokemonCard from "./PokemonCard";
 import PokemonFilter from "./PokemonFilter";
@@ -9,7 +9,7 @@ const PokemonList = () => {
   const [pokemonList, setPokemonList] = useState([]);
   const [nextUrl, setNextUrl] = useState();
   const [searchTerm, setSearchTerm] = useState("");
-  const [previousUrl, setPreviousUrl] = useState();
+  const loader = useRef(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,7 +18,7 @@ const PokemonList = () => {
 
       try {
         const response = await axios.get(
-          "https://pokeapi.co/api/v2/pokemon?offset=20&limit=30"
+          "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20"
         );
         const data = response.data;
         setNextUrl(data.next);
@@ -40,43 +40,46 @@ const PokemonList = () => {
     fetchData();
   }, []);
 
-  const nextPage = async () => {
-    let res = await fetch(nextUrl);
-    let data = await res.json();
-
-    setNextUrl(data.next);
-    setPreviousUrl(data.previous);
-
-    const newPokemonList = await Promise.all(
-      data.results.map(async (pokemon) => {
-        const response = await axios.get(pokemon.url);
-        const pokemonData = response.data;
-        return pokemonData;
-      })
-    );
-
-    setPokemonList(newPokemonList);
+  const handleObserver = (entries) => {
+    const target = entries[0];
+    if (target.isIntersecting && nextUrl) {
+      nextPage();
+    }
   };
 
-  const previousPage = async () => {
-    if (!previousUrl) return;
+  useEffect(() => {
+    const currentLoader = loader.current;
+    const observer = new IntersectionObserver(handleObserver, {
+      root: null,
+      rootMargin: "20px",
+      threshold: 1.0,
+    });
 
+    if (currentLoader) {
+      observer.observe(currentLoader);
+    }
+
+    return () => {
+      if (currentLoader) {
+        observer.unobserve(currentLoader);
+      }
+    };
+  }, [handleObserver]);
+
+  const nextPage = async () => {
     try {
-      const res = await axios.get(previousUrl);
-      const data = res.data;
-
+      const response = await axios.get(nextUrl);
+      const data = response.data;
       setNextUrl(data.next);
-      setPreviousUrl(data.previous);
 
-      const newPokemonList = await Promise.all(
+      const newPokemonData = await Promise.all(
         data.results.map(async (pokemon) => {
           const response = await axios.get(pokemon.url);
-          const pokemonData = response.data;
-          return pokemonData;
+          return response.data;
         })
       );
 
-      setPokemonList(newPokemonList);
+      setPokemonList((prevList) => [...prevList, ...newPokemonData]);
     } catch (error) {
       setError(error.message);
     }
@@ -91,7 +94,10 @@ const PokemonList = () => {
     if (isPokemonAlreadyAdded) {
       alert("Le pokemon existe déjà!");
     } else {
-      const updatedPokemonList = [...storedPokemonList, { name: pokemon.name, id: pokemon.id }];
+      const updatedPokemonList = [
+        ...storedPokemonList,
+        { name: pokemon.name, id: pokemon.id },
+      ];
       localStorage.setItem("pokemonList", JSON.stringify(updatedPokemonList));
       alert("Le pokemon a été ajouté avec succès!");
     }
@@ -101,20 +107,21 @@ const PokemonList = () => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredPokemonList = pokemonList.filter((pokemon) => {
+  const filteredPokemonList = pokemonList
+  .filter((pokemon) => {
     return pokemon.name.toLowerCase().includes(searchTerm.toLowerCase());
-  });
+  })
+  .reduce((unique, pokemon) => {
+    return unique.some((p) => p.name === pokemon.name) ? unique : [...unique, pokemon];
+  }, []);
 
   return (
     <div>
       {loading && <p>Loading...</p>}
-
       {error && <p>Error: {error}</p>}
-
       {!loading && !error && (
         <div>
           <PokemonFilter searchTerm={searchTerm} handleSearch={handleSearch} />
-
           {filteredPokemonList.map((pokemon) => (
             <div key={pokemon.name}>
               <PokemonCard key={pokemon.name} pokemon={pokemon} />
@@ -123,10 +130,11 @@ const PokemonList = () => {
               </button>
             </div>
           ))}
+          <div ref={loader} style={{ margin: "20px", textAlign: "center" }}>
+            {loading && <p>Loading...</p>}
+          </div>
         </div>
       )}
-      <button onClick={previousPage}>Page Precedente</button>
-      <button onClick={nextPage}>Page suivante</button>
     </div>
   );
 };
